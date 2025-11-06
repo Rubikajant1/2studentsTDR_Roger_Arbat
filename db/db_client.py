395b1ccf08919de76c2e0478521909744aeb9362
Rubikajant1 <164079350+Rubikajant1@users.noreply.github.com>
2025-11-06 03:11:34 +0100

    Use external string for MongoDB connection

--- db/db_client.py
+++ db/db_client.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
+from mongodb_str import string
 
-db_client = MongoClient("mongodbString")
+db_client = MongoClient(string)
 
 database = db_client["2Students_database"]
 
